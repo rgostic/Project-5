dd00ee5e86d4e144427c92263993a35ebaddc6fa
rgostic <rgostic@outlook.com>
2018-08-13 15:14:21 -0400

    Added other features!

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ def after_request(response):
 	''' close the db connection after each request'''
 	g.db.close()
 	return response
-
+@app.route('/')
 @app.route('/entries')
-def entries():
-	
+def entries():	
 	entries = models.Entry.select()
 	return render_template('index.html', entries=entries)
+
 
 @app.route('/entry', methods=["GET", "POST"])
 def entry():
@@ -36,12 +36,30 @@ def entry():
 		return redirect(url_for('entries'))
 	return render_template('new.html')
 
-@app.route('/details/<int:entry_id>')
-@app.route('/entries/<int:entry_id>')
-def details(entry_id):
-	entry = models.Entry.select().where(Entry.id == entry_id).get()
+@app.route('/edit/<int:entry_id>', methods=["GET", "POST"])
+@app.route('/entries/edit/<int:entry_id>', methods=["GET", "POST"])
+@app.route('/entry/<int:entry_id>', methods=["GET", "POST"])
+def edit(entry_id):
+	if request.method == "POST":
+		#update and save entry
+		entry = models.Entry.select().where(models.Entry.id == entry_id).get()
+		entry.title = request.form["title"]
+		entry.date = request.form["date"]
+		entry.time_spent = request.form["timeSpent"]
+		entry.learned = request.form["whatILearned"]
+		entry.resources = request.form["ResourcesToRemember"]
 
-	return render_template('detail', entry)
+		entry.save()
+		return redirect(url_for('entries', entry_id=entry_id))
+	entry = models.Entry.select().where(models.Entry.id == entry_id).get()
+	return render_template('edit.html', entry=entry)
+
+@app.route('/entries/<int:entry_id>')
+@app.route('/details/<int:entry_id>')
+def details(entry_id):
+	entry = models.Entry.select().where(models.Entry.id == entry_id).get()
+	return render_template('detail.html', entry=entry)
+
 
 
 if __name__ == "__main__":
